8fdd398cc53fbd4f9daff76bf82f404eb3d06b8f
Sobonga <smdlovu1@gmail.com>
2026-04-02 18:13:38 +0200

    json receiving formatt finished for all execept for the camera module

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+import mysql.connector as connector
 import json
 from datetime import datetime, timezone
 import pandas as pd
@@ -6,6 +7,8 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+#this is the place i intend for the database stuff to worked on in
+# connex = connector.connect(user="grp7_user", password="240_022", )
 
 
 def get_utc_iso_timestamp():
@@ -28,11 +31,15 @@ def home():
 Basic Data flow:
 1. ESP/sensor sends data to the webserver
 2. Server then formats the information to a clear measurement[cleaning]
-3. Then the dta is logged into the sql database for recording purposes
+3. Then the data is logged into the sql database for recording purposes
 4. Then after decisions are made with the data that has been captured [like turning on another sensor or sending a certain signal to that esp or even something in the server]
 5. the app route must end with an informative exit code
 
 """
+
+@app.route("/cam_module", methods=["GET", "POST"])
+def cam_module():
+    return 0
 
 @app.route("/pir_sensor", methods=["POST"])
 def pir_sensor():
@@ -45,9 +52,7 @@ def pir_sensor():
         {
         "type": "motion", # motion, light, temperature, humidity, distance [cam not included]
         "value": 19.598,  # motion will be a boolean 1 and 0
-        "alert_code": 0   # 0 = no issues, 1 = issues present
-        },
-    "timestamp": "2026-04-02T14:32:10Z"
+        }
     }
     """
 
@@ -63,20 +68,17 @@ def pir_sensor():
     #print(data)
     return {"status": "ok"}, 200
 
-@app.route("/cam_module", methods=["GET", "POST"])
-def cam_module():
-    return 0
-
 @app.route("/ldr_sensor", methods=["GET", "POST"])
 def ldr_sensor():
     """
     JSON:
-    device_id: <bridgette>,
+    device_id: <bridgette_esp>,
     readings:{
-        type:light_measure,
+        type:lux_reading,
         value:200
     }
     """
+
     data = request.json
 
     device_id = data.get("device_id")
@@ -91,11 +93,55 @@ def ldr_sensor():
 
 @app.route("/ultson_sensor", methods=["GET", "POST"])
 def ultson_sensor():
-    return 0
+    """
+    JSON:
+    device_id: <fanelo_esp>,
+    readings:{
+        type:meter_reading,
+        value:15cm
+    }
+    """
+    data = request.json
+
+    device_id = data.get("device_id")
+    readings = data.get("readings", {})
+
+    event_type = readings.get("type")
+    value = readings.get("value")
+
+    print(f"{device_id} | {event_type}: {value}")
+
+    return {"status": "ok"}, 200
 
 @app.route("/dht22_sensor", methods=["GET", "POST"])
 def dht22_sensor():
-    return 0
+    """
+    JSON:
+    {
+    device_id: <muzi_esp>,
+    readings:{[
+        {type:temperature_reading,
+        value:25},
+        {type:humidity_reading, value:}
+    ]}
+    }
+    """
+    data = request.json
+
+    device_id = data.get("device_id")
+    readings = data.get("readings", [])
+
+    #temperature reading
+    event_type = readings[0].get("type")
+    value = readings[0].get("value")
+
+    #humidity reading
+    event_type = readings[1].get("type")
+    value = readings[1].get("value")
+
+    print(f"{device_id} | {event_type}: {value}")
+
+    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
