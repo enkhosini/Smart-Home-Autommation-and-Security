99bbd06e94bc642edc5e2c8c741d96e942e1b380
Sobonga <smdlovu1@gmail.com>
2026-04-18 17:44:00 +0200

    changed the ip address

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def dht22_sensor():
     return {"status": "ok"}, 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="10.10.10.1", port=5000)
